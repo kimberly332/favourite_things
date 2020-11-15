5b4ab0e33e4c4959c30f77c4e8f595c15d07f732
kimberly332 <kimberly.wang1010@gmail.com>
2020-11-15 13:12:40 -0500

    finished adding lightbox

--- js/main.js
+++ js/main.js
@@ -1,53 +1,94 @@
 import {fetchData} from "./modules/DataMiner.js";
 
 (() => {
+
+    let db;
+    let count = 0; // count number of clicks - just for testing
+
     // stub * just a place for non-component-specific stuff
     console.log('loaded');
     
     let popUp = document.querySelector(".popup"),
         lbClose = popUp.querySelector("span"),
-        button1 = document.querySelector(".button1"),
-        button2 = document.querySelector(".button2"),
-        button3 = document.querySelector(".button3");
-
+        displaySection = document.querySelector(".display-section"),
+        name = document.querySelector(".name"),
+        selfIntro = document.querySelector(".selfIntroduction"),
+        popUpPhoto = document.querySelector(".photo");
 
     function popErrorBox(message) {
         alert("Something has gone horribly, horribly wrong");
     }
 
-    function handleDataSet(data) {
-        // populate a lightbox with this data
-        // and then open it
-        let lightbox = document.querySelector(".lightbox");
+    function showLightbox() {
+        popUp.classList.add('showPopup');
     }
 
-    function retrieveInfo (event) {
+    function hideLightbox() {
+        popUp.classList.remove('showPopup');
+    }
+
+    function retrieveInfo(event) {
+
+        count += 1;
+        console.log("click: ", count);
+        
+        try {
         // test for an ID
         // check for an id, and if there isn't one, then don't try the fetch call
         // because it'll break (the PHP will choke)
-        if (!event.target.id) {
-            return
-        }
 
-        fetchData(`./includes/index.php?id=${event.target.id}`).then(data => console.log(data)).catch(err => console.log(err));
+            showLightbox();
+        
+            // find out which info
+            const len = event.target.className.length;
+            const ind = parseInt(event.target.className[len - 1]) - 1;
+
+            // change content
+            console.log("changinggggggg");
+
+            name.textContent = db[ind].title;
+            selfIntro.textContent = db[ind].description;
+
+            // change image
+            popUpPhoto.src = `images/img${ind + 1}.jpg`;
+
+        } catch (error) {
+            console.log("oooops, cannot click!!!");
+            hideLightbox();
+        }
     }
 
     function renderImageThumbnails(thumbs) {
-        let userSection = document.querySelector('.user-section'),
-            userTemplate = document.querySelector('#user-template').content;
-        
+        console.log("inside renderImageThumbnails...");
+
+        let userTemplate = document.querySelector('#user-template').content;
+            
         for (let item in thumbs) {  
             let currentUser = userTemplate.cloneNode(true),
                 currentUserText = currentUser.querySelector(".item").children;
+    
+            currentUserText[2].src = `images/${thumbs[item].image}`;
 
-             currentUserText[1].src = `images/${thumbs[item].image}`;
-             currentUserText[1].id = thumbs[item].id;
+            // add "1"/"2"/"3" to class for later finding out which info in retreiveInfo function
+            currentUserText[2].classList.add(thumbs[item].id);
+            currentUserText[3].classList.add(thumbs[item].id);
+
             // add this new user to the view
-             userSection.appendChild(currentUser);
-         }
+            displaySection.appendChild(currentUser);
+        }
 
-         userSection.addEventListener("click", retrieveInfo);
+        displaySection.addEventListener("click", retrieveInfo);
+        
     }
 
-    fetchData("./includes/index.php").then(data => renderImageThumbnails(data)).catch(err => console.log(err));
+    lbClose.addEventListener("click", hideLightbox);
+
+    fetchData("./includes/index.php")
+        .then(data => {
+            db = data;
+            renderImageThumbnails(data);
+        })
+        .catch(err => console.log(err)
+    );
+
 })();
--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,16 +1,16 @@
 export default {
     Family: {
         title: "Family",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed elit sed enim pretium volutpat. Duis lobortis iaculis odio, quis pretium eros ullamcorper id. Mauris congue eget libero ut scelerisque."
+        description: "What does the word family mean to me? I am so lucky to have a very supportive family. Great parents, siblings and great friends and family. Family to me is the people that I don’t have to pretend with. The people that truly understand me. Family is the people that love me unconditionally."
     },
 
     Cats: {
         title: "Cats",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed elit sed enim pretium volutpat. Duis lobortis iaculis odio, quis pretium eros ullamcorper id. Mauris congue eget libero ut scelerisque."
+        description: "Cats, like human, are individuals. Each is unique, each has a unique personality. I have known cats I did not like at all, and can say the same about humans. However, I would trust the average cat a great deal more than I would trust the average human. Cats are real. They don’t lie, they don’t pretend to be something they are not. Cats repay respect with respect. Cats are my kind of people."
     },
 
     Travel: {
         title: "Travel",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed elit sed enim pretium volutpat. Duis lobortis iaculis odio, quis pretium eros ullamcorper id. Mauris congue eget libero ut scelerisque."
+        description: "Our lives are time-starved and technology-driven and, let’s be honest, it’s not often that we actually take the necessary time to de-stress and truly switch off. Travel allows us to escape life’s daily demands, dramas and deadlines and enables us to clear our minds. It encourages us to recharge our batteries and to truly disconnect from our phones, Wi-Fi, emails, laptops, social media, in order to reconnect with ourselves, each other and the natural environment."
     }
 }
